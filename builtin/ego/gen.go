823c521a65aa40fcd21ccf7bf04b55952d9ecd16
Nick Landolfi <nclandolfi@gmail.com>
2015-05-24 10:28:51 -0700

    move to templates.GenerateAndWriteGoFile

--- builtin/ego/gen.go
+++ builtin/ego/gen.go
@@ -3,9 +3,6 @@ package ego
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -27,41 +24,21 @@ func Generate(hostFile, root string) error {
 	routesFile := filepath.Join(root, "routes/routes.go")
 	routesContextFile := filepath.Join(root, "views/routes_context.go")
 
-	if err := ExecuteAndWrite(Core, coreFile, h); err != nil {
+	if err := templates.ExecuteAndWriteGoFile(engine, Core, coreFile, h); err != nil {
 		return err
 	}
 
-	if err := ExecuteAndWrite(Router, routerFile, h); err != nil {
+	if err := templates.ExecuteAndWriteGoFile(engine, Router, routerFile, h); err != nil {
 		return err
 	}
 
-	if err := ExecuteAndWrite(Routes, routesFile, h); err != nil {
+	if err := templates.ExecuteAndWriteGoFile(engine, Routes, routesFile, h); err != nil {
 		return err
 	}
 
-	if err := ExecuteAndWrite(RoutesContext, routesContextFile, h); err != nil {
+	if err := templates.ExecuteAndWriteGoFile(engine, RoutesContext, routesContextFile, h); err != nil {
 		return err
 	}
-
-	return nil
-}
-
-func ExecuteAndWrite(n templates.Name, file string, h *aeolus.Host) error {
-	var buf bytes.Buffer
-
-	if err := engine.Execute(&buf, n, h); err != nil {
-		return fmt.Errorf("error executing template: %s", err)
-	}
-
-	if err := ioutil.WriteFile(file, buf.Bytes(), 0644); err != nil {
-		return fmt.Errorf("error writing file: %s", err)
-	}
-
-	if err := exec.Command("goimports", "-w=true", file).Run(); err != nil {
-		return fmt.Errorf("error running goimports on file: %s", err)
-	}
-
-	log.Printf("wrote %s", file)
 
 	return nil
 }
